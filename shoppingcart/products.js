01e513719bf41051f363dd3d27ea803e92b952e4
fallouua92 <91126667+fallouua92@users.noreply.github.com>
2021-10-19 09:06:45 +0100

    update

---
+++ shoppingcart/products.js
@@ -0,0 +1,29 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+    123: {
+      name : "Lucy",
+      desc : "A very helpful dog you will definitely love it.",
+      img : "dog-two",
+      price : 1000
+    },
+    124: {
+      name : "MokBook Rookie",
+      desc : "Unsatiable its possession nor off.",
+      img : "dummy-pdt-b.jpg",
+      price : 1247
+    },
+    125: {
+      name : "iPong Max",
+      desc : "All difficulty unreserved the solicitude.",
+      img : "dummy-pdt-a.jpg",
+      price : 675
+    },
+    126: {
+      name : "iTab Pok",
+      desc : "Had judgment out property the supplied. ",
+      img : "dummy-pdt-a.jpg",
+      price : 842
+    }
+  };
+
+  
