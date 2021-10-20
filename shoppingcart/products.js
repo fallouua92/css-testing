37790f228ee57dac747e0ab310347906c13c5e97
fallouua92 <91126667+fallouua92@users.noreply.github.com>
2021-10-20 10:00:07 +0100

    update

--- shoppingcart/products.js
+++ shoppingcart/products.js
@@ -3,28 +3,24 @@ var productswebpets = {
     123: {
       name : "Lucy",
       desc : "A very helpful dog you will definitely love it.",
-      category:"Dog",
       img : "dog-two.jpg",
       price : 1000
     },
     124: {
       name : "Lucy",
       desc : "A great dog to play with it.",
-      category :"Dog",
       img : "dog-three.jpg",
       price : 900
     },
     125: {
       name : "Milo",
       desc : "A wonderful cat to feel right at home.",
-      category :"cat",
       img : "cat-three.jpg",
       price : 500
     },
     126: {
       name : "Leo",
       desc : "A great cat to chill with. ",
-      category :"cat",
       img : "cat-four.jpg",
       price : 700
     }
